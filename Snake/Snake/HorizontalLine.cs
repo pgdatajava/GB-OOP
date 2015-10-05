ddc3b2867820ff6a72180b5f4b44c1c450459801
admin <pgdatajava@gmail.com>
2015-10-05 20:07:38 +0600

    class HorizontalLine comments

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    //draw horizontal line
+    class HorizontalLine
+    {
+        //create List
+        List<Point> pList;
+
+        //constructor
+        public HorizontalLine()
+        {
+            pList = new List<Point>();
+            //create emty List
+            Point p1 = new Point(4, 4, '*');
+            Point p2 = new Point(5, 4, '*');
+            Point p3 = new Point(6, 4, '*');
+            //add Point objects into the List
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+
+        }
+
+        //drawing method
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                //calling Point object's function
+                p.Draw();
+            }
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -18,32 +18,9 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            //class List
-            List<int> numList = new List<int>();
-            //add elements in the List
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            //cycle
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            //remove the element from the List
-            numList.Remove(0);
-
-            //create List of Point class objects
-            List<Point> pList = new List<Point>();
-            //add existing Point objects
-            pList.Add(p1);
-            pList.Add(p2);
-
+            //create HorizontalLine object
+            HorizontalLine line = new HorizontalLine();
+            line.Draw();
 
             Console.ReadKey();
         }
